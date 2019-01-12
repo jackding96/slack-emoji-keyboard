5fa9dec9c3553409b9560210e82e8ec8d948dae6
jack.ding <jackding96@outlook.com>
2019-01-12 18:22:03 -0500

    change shortcut

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-const { app, BrowserWindow } = require('electron')
+const electron = require('electron');
+const { app, BrowserWindow, globalShortcut } = require('electron');
 
 function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  globalShortcut.register(':', () => {
+    // Create the browser window.
+    win = new BrowserWindow({ width: 300, height: 300, x: electron.screen.getCursorScreenPoint().x, y: electron.screen.getCursorScreenPoint().y });
 
-  // and load the index.html of the app.
-  win.loadFile('index.html')
+    console.log(electron.screen.getCursorScreenPoint());   
+    
+    // and load the index.html of the app.
+    win.loadFile('index.html');
+  })
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
